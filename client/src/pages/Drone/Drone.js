49f7ba66524adbd4a37748ae05851a99d6a98b30
Brian Martin <Brian Martin>
2017-10-04 09:44:39 -0400

    states and functions added to hurricane and drone page, modals not working yet

--- client/src/pages/Drone/Drone.js
+++ client/src/pages/Drone/Drone.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
-import { Link } from "react-router-dom";
-import { List, ListItem } from "../../components/List";
+import API from "../../utils/API";
 import { Col, Row, Container } from "../../components/Grid";
 import { Input, FormBtn } from "../../components/Form";
 import "./Drone.css";
--- client/src/pages/Hurricane/Hurricane.js
+++ client/src/pages/Hurricane/Hurricane.js
@@ -1,19 +1,59 @@
 import React, { Component } from "react";
-import { Link } from "react-router-dom";
-import { List, ListItem } from "../../components/List";
+import API from "../../utils/API";
 import { Col, Row, Container } from "../../components/Grid";
+import { Input, FormBtn } from "../../components/Form";
 import "./Hurricane.css";
 
 class Hurricane extends Component{
+
+    state = {
+        Hurricane: [],
+        title: "",
+        author: "",
+        link: "",
+        body: "",
+    }
+
+    componentDidMount() {
+        this.loadHurricane();
+    }
+
+    loadHurricane = () => {
+        API.getHurricane()
+        .then(res =>
+            this.setState({ Hurricane: res.data, title: "", author: "", link: "", body: "" })
+        ).catch(err => console.log(err));
+    };
+
+    handleInputChange = event => {
+        const {name, value } = event.target;
+        this.setState({
+            [name]: value
+        });
+    };
+
+    handleFormSubmit = event => {
+        event.preventDefault();
+        if(this.state.title && this.state.author && this.state.body) {
+            API.saveHurricane({
+                title: this.state.title,
+                author: this.state.author,
+                link: this.state.link,
+                author: this.state.author
+            }).then(res => this.loadDrone())
+            .catch(err => console.log(err));
+        }
+    }
 
     render(){
         return(
             <div>
                 <Container fluid>
-                    <h1>Hurricane Prep</h1><br/>
+                    <h1>Hurricane Prep</h1>
                     <div className="wrapper">
-                        <button className="btn btn-default btn-lg">Add Your Post</button><br/>
+                        <FormBtn className="btn btn-default btn-lg">Add Your Post</FormBtn>
                     </div>
+                    <br/> <br/> <br/>
                     <Row>
                         <Col size="md-2"/>
                         <Col size="md-8">
