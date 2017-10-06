d4a833110a1f79d86266c9f77471c6b21e274f86
Brian Martin <Brian Martin>
2017-10-06 13:03:23 -0400

    app running

---
+++ client/src/utils/API.js
@@ -0,0 +1,30 @@
+import axios from "axios";
+
+export default {
+    //Get user with the given id
+    getUser: function(id) {
+        return axios.get("/api/users/" + id);
+    },
+
+    //Save user to the database
+    saveUser: function(userData) {
+        return axios.post("/api/users", userData);
+    },
+
+    //Get drone with given id
+    getDrone: function(id) {
+        return axios.get("/api/drones/" + id);
+    },
+
+    saveDrone: function(droneData) {
+        return axios.post("/api/drones", droneData);
+    },
+
+    getHurricane: function(id) {
+        return axios.get("/api/hurricanes/" + id);
+    },
+
+    saveHurricane: function(hurricaneData) {
+        return axios.post("/api/hurricanes", hurricaneData);
+    },
+};
