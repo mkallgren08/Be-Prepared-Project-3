423bf0657989e090dfef4b2cdacd66436ab38e4e
Renee Geffre <renee.geffre@gmail.com>
2017-10-13 14:23:28 -0400

    fixes for build

--- client/src/auth/auth0-variables.js
+++ client/src/auth/auth0-variables.js
@@ -1,6 +1,6 @@
 export const AUTH_CONFIG = {
     domain: "rgtechnologygroup.auth0.com",
     clientId: "wcp3Q8nYvSde0kzv9HSR4m5Mzw5Y2tVv",
-    callbackUrl: "http://polaris-disaster-prep.herokuapp.com/callback",
+    callbackUrl: "https://polaris-disaster-prep.herokuapp.com/callback",
     apiUrl: "http://localhost:3001/api"
   };
