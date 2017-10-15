c2b0a01610ba774a6af649a766512749bb3b0aff
Michael <mkallgren08@gmail.com>
2017-10-15 01:02:24 -0400

    Added more stylings to the home page that can also be applied to panels on other pages

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -13,32 +13,40 @@ const Home = () =>
             <Row>
                 <Col size="md-3" />
                 <Col size="md-6">
-                    <strong>
-                    <div className="whiteText intro"><h2 style={{textDecoration: "underline"}}>Welcome to the Polaris emergency preparedness site</h2> 
-                        <p>Emergencies and disasters can happen at any time.  Being prepared will provide you and your family
+                    <div className="homePageBody">
+                        <strong>
+                            <div className="intro"><h2 style={{ textDecoration: "underline" }}>Welcome to the Polaris Preparedness</h2>
+                                <p className="introText">Emergencies and disasters can happen at any time.  Being prepared will provide you and your family
                         with what you need to cope with those emergencies.  This site will provide you with information
                         and resources to help you prepare for and manage emergencies.</p>
+                            </div>
+                        </strong>
                     </div>
-                    </strong>
                 </Col>
                 <Col size="md=3" />
             </Row>
             <br /><br />
             <Row>
-                <Col size="md-4" />
-                <Col size="md-4">
-                    <Jumbotron>
+                <Col size="md-2" />
+                <Col size="md-8">
+                    <div>
                         <h2 style={{ color: 'white', textAlign: 'center', textDecoration: "underline" }}>Emergency Checklists</h2>
-                        <a href="./uploads/CommuterEmergencyPlan.pdf" target="_blank"><h4><span className="glyphicon glyphicon-list-alt" /> Commuter Emergency Plan</h4></a>
-                        <a href="./uploads/DisasterPlanningforPetOwners.pdf" target="_blank"><h4><span className="glyphicon glyphicon-list-alt" /> Disaster Planning for Pet Owners</h4></a>
-                        <a href="./uploads/DisasterPlanningforSpecialNeeds.pdf" target="_blank"><h4><span className="glyphicon glyphicon-list-alt" /> Disaster Planning for Special Needs</h4></a>
-                        <a href="./uploads/EmergencyChecklist2014.pdf" target="_blank"><h4><span className="glyphicon glyphicon-list-alt" /> Emergency Checklist 2012</h4></a>
-                        <a href="./uploads/FamilyCommunicationplankids.pdf" target="_blank"><h4><span className="glyphicon glyphicon-list-alt" /> Family Communication Plan for Kids</h4></a>
-                        <a href="./uploads/FamilyCommunicationPlanparents.pdf" target="_blank"><h4><span className="glyphicon glyphicon-list-alt" /> Family Communication Plan for Parents</h4></a>
-                        <a href="./uploads/FEMAChecklistChild.pdf" target="_blank"><h4><span className="glyphicon glyphicon-list-alt" /> FEMA Checklist</h4></a>
-                    </Jumbotron>
+                        <Row>
+                            <Col size="md-6">
+                                <a href="./uploads/CommuterEmergencyPlan.pdf" target="_blank"><h4><span className="glyphicon glyphicon-list-alt" /> Commuter Emergency Plan</h4></a>
+                                <a href="./uploads/DisasterPlanningforPetOwners.pdf" target="_blank"><h4><span className="glyphicon glyphicon-list-alt" /> Disaster Planning for Pet Owners</h4></a>
+                                <a href="./uploads/DisasterPlanningforSpecialNeeds.pdf" target="_blank"><h4><span className="glyphicon glyphicon-list-alt" /> Disaster Planning for Special Needs</h4></a>
+                                <a href="./uploads/EmergencyChecklist2014.pdf" target="_blank"><h4><span className="glyphicon glyphicon-list-alt" /> Emergency Checklist 2012</h4></a>
+                            </Col>
+                            <Col size="md-6">
+                                <a href="./uploads/FamilyCommunicationplankids.pdf" target="_blank"><h4><span className="glyphicon glyphicon-list-alt" /> Family Communication Plan for Kids</h4></a>
+                                <a href="./uploads/FamilyCommunicationPlanparents.pdf" target="_blank"><h4><span className="glyphicon glyphicon-list-alt" /> Family Communication Plan for Parents</h4></a>
+                                <a href="./uploads/FEMAChecklistChild.pdf" target="_blank"><h4><span className="glyphicon glyphicon-list-alt" /> FEMA Checklist</h4></a>
+                            </Col>
+                        </Row>
+                    </div>
                 </Col>
-                <Col size="md-4" />
+                <Col size="md-2" />
             </Row>
         </Container>;
     </div>
